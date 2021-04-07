d5fbe9fb6e03717bddb5a0e4f2de2410bf6e98d3
HoroWise <79516164+HoroWise@users.noreply.github.com>
2021-04-07 18:57:33 +0500

    Класс Point3D

--- AydarOOP2/AydarOOP2.cpp
+++ AydarOOP2/AydarOOP2.cpp
@@ -3,9 +3,9 @@
 using namespace std; 
 
 class Point {
-public:
+protected:
 	int x, y;
-
+public:
 	Point() {
 		x = 0;
 		y = 0;
@@ -44,10 +44,44 @@ public:
 void Point::reset() {
 	x = 0;
 	y = 0;
-	printf("Запустился метод reset у класса Point\n");
+	printf("Запуск метода reset у класса Point\n");
 	printf("Координаты точки (%d; %d)\n", x, y);
 }
 
+class Point3D : public Point {
+protected:
+	int z;
+public:
+	Point3D() : Point() {
+		z = 0;
+		printf("Запуск конструктора по умолчанию у класса Point3D\n");
+	}
+
+	Point3D(int x, int y, int z) : Point(x, y) {
+		this->z = z;
+		printf("Запуск конструктора c параметрами у класса Point3D\n");
+		printf("Координаты точки (%d;%d;%d)\n", x, y, z);
+	}
+
+	Point3D(const Point3D& p3D) {
+		x = p3D.x;
+		y = p3D.y;
+		z = p3D.z;
+		printf("Запуск конструктора копирования у класса Point3D\n");
+		printf("Координаты точки (%d;%d;%d)\n", x, y, z);
+	}
+	void move(int mx, int my, int mz) { // Реализация метода в определении класса
+		x += mx;
+		y += my;
+		z += mz;
+		printf("Запуск метод move у класса Point3D\n");
+		printf("Координаты точки (%d;%d;%d)\n", x, y, z);
+	}
+	~Point3D() {
+		printf("\nЗапуск деструктора у класса Point3D\n");
+		printf("Последние координаты точки (%d;%d;%d)\n", x, y, z);
+	}
+};
 
 int main()
 {
